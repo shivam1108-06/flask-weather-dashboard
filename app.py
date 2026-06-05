054129783f3d17d9d7aee7b0d587da6fafb4d947
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-05 20:10:25 +0530

    Weather API Integration

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+API_KEY = "YOUR_API_KEY"
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    weather = None
+
+    if request.method == "POST":
+
+        city = request.form["city"]
+
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        if response.status_code == 200:
+
+            weather = {
+                "city": data["name"],
+                "temp": data["main"]["temp"],
+                "description": data["weather"][0]["description"]
+            }
+
+    return render_template(
+        "index.html",
+        weather=weather
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
